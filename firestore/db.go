a717827e7fe2dfd6f5c643f8a076af73c784017d
perebaj <perebaj@gmail.com>
2024-08-16 12:28:17 -0300

    :bug: lint

--- firestore/db.go
+++ firestore/db.go
@@ -4,12 +4,14 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
+// Storage is a struct that holds the firestore client and the projectID and database name
 type Storage struct {
 	client    *firestore.Client
 	projectID string
 	database  string
 }
 
+// NewStorage creates a new Storage struct
 func NewStorage(client *firestore.Client, projectID, database string) *Storage {
 	return &Storage{
 		client:    client,
