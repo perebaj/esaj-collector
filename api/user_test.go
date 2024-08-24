bfb4850a506b935621f7646832e18241d3a98e19
perebaj <perebaj@gmail.com>
2024-08-24 16:38:43 -0300

    :sparkles: improvement update user unit test

--- api/user_test.go
+++ api/user_test.go
@@ -66,6 +66,66 @@ func TestUserHandler_ClerkWebHookHandler_errorDecoding(t *testing.T) {
 	require.Equal(t, 400, w.Code)
 }
 
+func TestUserHandler_ClerkWebHookHandler_updateUser(t *testing.T) {
+	const event = `{
+		"data": {
+		  "birthday": "",
+		  "created_at": 1654012591514,
+		  "email_addresses": [
+			{
+			  "email_address": "example@example.org",
+			  "id": "idn_29w83yL7CwVlJXylYLxcslromF1",
+			  "linked_to": [],
+			  "object": "email_address",
+			  "reserved": true,
+			  "verification": {
+				"attempts": null,
+				"expire_at": null,
+				"status": "verified",
+				"strategy": "admin"
+			  }
+			}
+		  ],
+		  "external_accounts": [],
+		  "external_id": null,
+		  "first_name": "Example",
+		  "gender": "",
+		  "id": "user_29w83sxmDNGwOuEthce5gg56FcC",
+		  "image_url": "https://img.clerk.com/xxxxxx",
+		  "last_name": null,
+		  "last_sign_in_at": null,
+		  "object": "user",
+		  "password_enabled": true,
+		  "phone_numbers": [],
+		  "primary_email_address_id": "idn_29w83yL7CwVlJXylYLxcslromF1",
+		  "primary_phone_number_id": null,
+		  "primary_web3_wallet_id": null,
+		  "private_metadata": {},
+		  "profile_image_url": "https://www.gravatar.com/avatar?d=mp",
+		  "public_metadata": {},
+		  "two_factor_enabled": false,
+		  "unsafe_metadata": {},
+		  "updated_at": 1654012824306,
+		  "username": null,
+		  "web3_wallets": []
+		},
+		"object": "event",
+		"type": "user.updated"
+	  }`
+
+	ctrl := gomock.NewController(t)
+	userStorageMock := mock.NewMockUserStorage(ctrl)
+	userStorageMock.EXPECT().SaveUser(gomock.Any(), gomock.Any()).Return(nil)
+
+	req := httptest.NewRequest("POST", "/", strings.NewReader(event))
+	w := httptest.NewRecorder()
+
+	userHandler := api.NewUserHandler(userStorageMock)
+	userHandler.ClerkWebHookHandler(w, req)
+
+	require.Equal(t, 200, w.Code)
+}
+
 func TestUserHandler_ClerkWebHookHandler_deleteUser(t *testing.T) {
 	const event = `{
 		"data": {
