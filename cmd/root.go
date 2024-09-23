fb3f8ec7aa17a00a24d2534f39e281bf31c9baf8
perebaj <perebaj@gmail.com>
2024-09-22 23:15:24 -0300

    :sparkles: search by oab and save on file

--- cmd/root.go
+++ cmd/root.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/perebaj/esaj/esaj"
+	"github.com/schollz/progressbar/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -28,6 +29,7 @@ var collectCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, _ []string) {
 		oab, _ := cmd.Flags().GetString("oab")
 		processID, _ := cmd.Flags().GetString("process")
+		output, _ := cmd.Flags().GetString("output")
 		ctx := cmd.Context()
 		if oab == "" && processID == "" {
 			fmt.Println("Error: You must provide either an OAB number or a process ID")
@@ -35,28 +37,64 @@ var collectCmd = &cobra.Command{
 			return
 		}
 
-		foro, err := esaj.ForoNumeroUnificado(processID)
-		if err != nil {
-			fmt.Println("Error getting foro:", err)
-			return
-		}
 		eClient := esaj.New(esaj.Config{}, &http.Client{
 			Timeout: 30 * time.Second,
 		})
 
 		if oab != "" {
 			fmt.Println("Collecting data for OAB number:", oab)
-			// Add your logic for OAB collection here
+			seed, err := eClient.SearchByOAB(ctx, oab)
+			if err != nil {
+				fmt.Println("Error searching by OAB:", err)
+				return
+			}
+			bar := progressbar.Default(int64(len(seed)))
+			var allProcesses []esaj.ProcessBasicInfo
+			for _, s := range seed {
+				processBasicInfo, err := eClient.FetchBasicProcessInfo(ctx, s.URL, s.ProcessID)
+				if err != nil {
+					fmt.Println("Error fetching basic process info:", err)
+					return
+				}
+				processBasicInfo.OAB = oab
+				allProcesses = append(allProcesses, *processBasicInfo)
+				_ = bar.Add(1)
+			}
+
+			data, err := json.Marshal(allProcesses)
+			if err != nil {
+				fmt.Println("Error marshalling basic process info:", err)
+				return
+			}
+
+			f, err := os.Create(output)
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				return
+			}
+			defer func() {
+				_ = f.Close()
+			}()
+			_, err = f.Write(data)
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				return
+			}
 		}
 
-		if processID != "" && foro != "" {
+		if processID != "" {
+			foro, err := esaj.ForoNumeroUnificado(processID)
+			if err != nil {
+				fmt.Println("Error getting foro:", err)
+				return
+			}
 			fmt.Println("Collecting data for Process ID:", processID)
 			processCode, err := eClient.ProcessCodeByProcessID(processID)
 			if err != nil {
 				fmt.Println("Error getting process code:", err)
 				return
 			}
-			url := fmt.Sprintf("https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=%s&processo.foro=%s", processCode, processID)
+			url := fmt.Sprintf("https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=%s&processo.foro=%s", processCode, foro)
 			processBasicInfo, err := eClient.FetchBasicProcessInfo(ctx, url, processID)
 			if err != nil {
 				fmt.Println("Error fetching basic process info:", err)
@@ -78,7 +116,7 @@ func init() {
 	rootCmd.AddCommand(downloadCmd)
 	collectCmd.Flags().StringP("oab", "o", "", "OAB number to search")
 	collectCmd.Flags().StringP("process", "p", "", "Process ID to search")
-
+	collectCmd.Flags().StringP("output", "O", "processes.json", "Output file")
 }
 
 var downloadCmd = &cobra.Command{
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/cloudevents/sdk-go/v2 v2.15.2
 	github.com/googleapis/google-cloudevents-go v0.8.0
 	github.com/sashabaranov/go-openai v1.27.1
+	github.com/schollz/progressbar/v3 v3.15.0
 	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/mock v0.4.0
@@ -45,10 +46,12 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.49.0 // indirect
@@ -63,7 +66,8 @@ require (
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/term v0.24.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/api v0.189.0 // indirect
